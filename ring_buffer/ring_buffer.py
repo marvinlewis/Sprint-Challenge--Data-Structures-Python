b80104002fc4f6411013e5857c9ad5a5f8700536
Marvin  Lewis <marvinlewis@marvins-air.home>
2020-09-15 16:58:15 -0700

    buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,18 @@
 class RingBuffer:
-    def __init__(self, capacity):
-        pass
+  def __init__(self, capacity):
+    self.capacity = capacity
+    self.current = 0
+    self.storage = [None]*capacity
 
-    def append(self, item):
-        pass
+  def append(self, item):
+    self.storage[self.current] = item
+    if self.current == self.capacity - 1:
+      self.current = 0
+    else:
+      self.current += 1
 
-    def get(self):
-        pass
+  def get(self): 
+    if self.storage[-1] == None:
+      return self.storage[:self.current]
+    else:
+      return self.storage
